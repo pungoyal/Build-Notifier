a394e47f10fb2d0d3fd5667d4a20a95e076aeb84
Puneet Goyal <pungoyal@gmail.com>
2010-09-10 15:51:49 +0530

    fixing project setup

--- app/src/com/thoughtworks/buildnotifier/BuildNotifier.java
+++ app/src/com/thoughtworks/buildnotifier/BuildNotifier.java
@@ -3,13 +3,11 @@ package com.thoughtworks.buildnotifier;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class BuildNotifier extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class BuildNotifier extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
