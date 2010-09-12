e2320518e1fcb09d779fe1086f0892308d75d006
Puneet Goyal <pungoyal@gmail.com>
2010-09-12 21:10:30 +0530

    porting projects to idea.

--- unit/src/com/thoughtworks/buildnotifier/domain/PipelinesTest.java
+++ unit/src/com/thoughtworks/buildnotifier/domain/PipelinesTest.java
@@ -1,15 +1,15 @@
 package com.thoughtworks.buildnotifier.domain;
 
-import static org.junit.Assert.assertNull;
-
 import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
 
 public class PipelinesTest {
 
-	@Test
-	public void testFind() {
-		Pipelines pipelines = new Pipelines();
-		Pipeline pipeline = pipelines.find("project");
-		assertNull(pipeline);
-	}
+    @Test
+    public void testFind() {
+        Pipelines pipelines = new Pipelines();
+        Pipeline pipeline = pipelines.find("project");
+        assertNotNull(pipeline);
+    }
 }
