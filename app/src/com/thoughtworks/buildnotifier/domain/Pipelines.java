8234a8535b5ad6041746fa5f29fa64503cd9dc39
Puneet Goyal <pungoyal@gmail.com>
2010-09-14 17:48:52 +0530

    fixing parsing logic

--- app/src/com/thoughtworks/buildnotifier/domain/Pipelines.java
+++ app/src/com/thoughtworks/buildnotifier/domain/Pipelines.java
@@ -3,34 +3,35 @@ package com.thoughtworks.buildnotifier.domain;
 import java.util.ArrayList;
 
 public class Pipelines extends ArrayList<Pipeline> {
-	private static final long serialVersionUID = 7224490213190557979L;
+    private static final long serialVersionUID = 7224490213190557979L;
 
-	public Pipeline findOrCreate(String name) {
-		for (Pipeline pipeline : this) {
-			if (pipeline.equals(name))
-				return pipeline;
-		}
-		Pipeline pipeline = new Pipeline(name);
-		add(pipeline);
-		return pipeline;
-	}
+    public Pipeline findOrCreate(String name) {
 
-	public void addStage(String pipelineName, Stage stage) {
-		Pipeline pipeline = findOrCreate(pipelineName);
-		pipeline.addStage(stage);
-	}
+        for (Pipeline pipeline : this) {
+            if (pipeline.getName().equals(name))
+                return pipeline;
+        }
+        Pipeline pipeline = new Pipeline(name);
+        add(pipeline);
+        return pipeline;
+    }
 
-	public void addJobToStage(String pipelineName, String stageName, Job job) {
-		Pipeline pipeline = findOrCreate(pipelineName);
-		pipeline.addJobToStage(stageName, job);
-	}
+    public void addStage(String pipelineName, Stage stage) {
+        Pipeline pipeline = findOrCreate(pipelineName);
+        pipeline.addStage(stage);
+    }
 
-	public Pipeline find(String name) {
-		for (Pipeline pipeline : this) {
-			if (pipeline.equals(name))
-				return pipeline;
-		}
-		return new Pipeline(name);
-	}
+    public void addJobToStage(String pipelineName, String stageName, Job job) {
+        Pipeline pipeline = findOrCreate(pipelineName);
+        pipeline.addJobToStage(stageName, job);
+    }
+
+    public Pipeline find(String name) {
+        for (Pipeline pipeline : this) {
+            if (pipeline.getName().equals(name))
+                return pipeline;
+        }
+        return new Pipeline(name);
+    }
 
 }
--- app/src/com/thoughtworks/buildnotifier/factories/PipelineFactory.java
+++ app/src/com/thoughtworks/buildnotifier/factories/PipelineFactory.java
@@ -1,55 +1,49 @@
 package com.thoughtworks.buildnotifier.factories;
 
+import com.thoughtworks.buildnotifier.domain.*;
+import org.xml.sax.Attributes;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.xml.sax.Attributes;
-
-import com.thoughtworks.buildnotifier.domain.BuildActivity;
-import com.thoughtworks.buildnotifier.domain.BuildStatus;
-import com.thoughtworks.buildnotifier.domain.Constants;
-import com.thoughtworks.buildnotifier.domain.Job;
-import com.thoughtworks.buildnotifier.domain.Pipelines;
-import com.thoughtworks.buildnotifier.domain.Stage;
-
 public class PipelineFactory {
-	private Pipelines pipelines;
+    private Pipelines pipelines;
 
-	public PipelineFactory() {
-		pipelines = new Pipelines();
-	}
+    public PipelineFactory() {
+        pipelines = new Pipelines();
+    }
 
-	public void parseAndAdd(Attributes attributes) {
-		BuildActivity activity = BuildActivity.parse(attributes.getValue("activity"));
-		BuildStatus lastBuildStatus = BuildStatus.parse(attributes.getValue("lastBuildStatus"));
-		String lastBuildLabel = attributes.getValue("lastBuildLabel");
+    public void parseAndAdd(Attributes attributes) {
+        BuildActivity activity = BuildActivity.parse(attributes.getValue("activity"));
+        BuildStatus lastBuildStatus = BuildStatus.parse(attributes.getValue("lastBuildStatus"));
+        String lastBuildLabel = attributes.getValue("lastBuildLabel");
 
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd'T'HH:mm:ss");
-		Date lastBuildTime;
-		try {
-			lastBuildTime = sdf.parse(attributes.getValue("lastBuildTime"));
-		} catch (ParseException e) {
-			e.printStackTrace();
-			lastBuildTime = new Date();
-		}
-		String url = attributes.getValue("webUrl");
-		String name = attributes.getValue("name");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd'T'HH:mm:ss");
+        Date lastBuildTime;
+        try {
+            lastBuildTime = sdf.parse(attributes.getValue("lastBuildTime"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            lastBuildTime = new Date();
+        }
+        String url = attributes.getValue("webUrl");
+        String name = attributes.getValue("name");
 
-		String[] buildables = name.split(Constants.SEPARATOR);
-		String pipelineName = buildables[0];
-		String stageName = buildables[1];
+        String[] buildables = name.split(Constants.SEPARATOR);
+        String pipelineName = buildables[0];
+        String stageName = buildables[1];
 
-		if (buildables.length == 2) {
-			Stage stage = new Stage(stageName, activity, lastBuildStatus, lastBuildLabel, lastBuildTime, url);
-			pipelines.addStage(pipelineName, stage);
-		} else {
-			Job job = new Job(buildables[2], activity, lastBuildStatus, lastBuildLabel, lastBuildTime, url);
-			pipelines.addJobToStage(pipelineName, stageName, job);
-		}
-	}
+        if (buildables.length == 2) {
+            Stage stage = new Stage(stageName, activity, lastBuildStatus, lastBuildLabel, lastBuildTime, url);
+            pipelines.addStage(pipelineName, stage);
+        } else {
+            Job job = new Job(buildables[2], activity, lastBuildStatus, lastBuildLabel, lastBuildTime, url);
+            pipelines.addJobToStage(pipelineName, stageName, job);
+        }
+    }
 
-	public Pipelines getPipelines() {
-		return pipelines;
-	}
+    public Pipelines getPipelines() {
+        return pipelines;
+    }
 }
