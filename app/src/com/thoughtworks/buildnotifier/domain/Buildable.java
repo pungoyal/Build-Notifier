63b34a0793049aed936787eeeddfe528d1d693d1
Puneet Goyal <pungoyal@gmail.com>
2010-09-14 17:39:26 +0530

    changes

--- app/src/com/thoughtworks/buildnotifier/domain/Buildable.java
+++ app/src/com/thoughtworks/buildnotifier/domain/Buildable.java
@@ -4,50 +4,50 @@ import java.util.Date;
 
 public abstract class Buildable {
 
-	private final String name;
-	private final BuildActivity activity;
-	private final BuildStatus lastBuildStatus;
-	private final String lastBuildLabel;
-	private final Date lastBuildTime;
-	private final String url;
+    private final String name;
+    private final BuildActivity activity;
+    private final BuildStatus lastBuildStatus;
+    private final String lastBuildLabel;
+    private final Date lastBuildTime;
+    private final String url;
 
-	protected Buildable(String name, BuildActivity activity, BuildStatus lastBuildStatus, String lastBuildLabel,
-			Date lastBuildTime, String url) {
-		this.name = name;
-		this.activity = activity;
-		this.lastBuildStatus = lastBuildStatus;
-		this.lastBuildLabel = lastBuildLabel;
-		this.lastBuildTime = lastBuildTime;
-		this.url = url;
-	}
+    protected Buildable(String name, BuildActivity activity, BuildStatus lastBuildStatus, String lastBuildLabel,
+                        Date lastBuildTime, String url) {
+        this.name = name;
+        this.activity = activity;
+        this.lastBuildStatus = lastBuildStatus;
+        this.lastBuildLabel = lastBuildLabel;
+        this.lastBuildTime = lastBuildTime;
+        this.url = url;
+    }
 
-	@Override
-	public String toString() {
-		return getName() + ":" + getActivity() + ":" + getLastBuildStatus() + ":" + getLastBuildLabel() + ":"
-				+ getLastBuildTime() + ":" + getUrl();
-	}
+    @Override
+    public String toString() {
+        return getName() + ":" + getActivity() + ":" + getLastBuildStatus() + ":" + getLastBuildLabel() + ":"
+                + getLastBuildTime() + ":" + getUrl();
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public BuildStatus getLastBuildStatus() {
-		return lastBuildStatus;
-	}
+    public BuildStatus getLastBuildStatus() {
+        return lastBuildStatus;
+    }
 
-	public String getLastBuildLabel() {
-		return lastBuildLabel;
-	}
+    public String getLastBuildLabel() {
+        return lastBuildLabel;
+    }
 
-	public Date getLastBuildTime() {
-		return lastBuildTime;
-	}
+    public Date getLastBuildTime() {
+        return lastBuildTime;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public BuildActivity getActivity() {
-		return activity;
-	}
+    public BuildActivity getActivity() {
+        return activity;
+    }
 }
