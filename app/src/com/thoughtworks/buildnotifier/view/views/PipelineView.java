a208d0bdfd79070cc38ed650afca5cce0a55a4fe
Garima Singh <garima@ThoughtWorks.(none)>
2010-09-15 16:57:26 +0530

    New stage status type

--- app/src/com/thoughtworks/buildnotifier/view/views/PipelineView.java
+++ app/src/com/thoughtworks/buildnotifier/view/views/PipelineView.java
@@ -2,6 +2,7 @@ package com.thoughtworks.buildnotifier.view.views;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import com.thoughtworks.buildnotifier.R;
@@ -16,17 +17,12 @@ public class PipelineView extends LinearLayout {
 
         Button stageButton = new Button(context);
         stageButton.setText(stage.getName());
-        stageButton.setTextColor(getTextColor(stage.getLastBuildStatus(), stage.getActivity()));
+        stageButton.setTextColor(Color.BLACK);
+        stageButton.setTypeface(null, Typeface.BOLD);
         stageButton.setBackgroundResource(getResource(stage.getLastBuildStatus(), stage.getActivity()));
 
         addView(stageButton, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
 
-    }
-
-    private int getTextColor(BuildStatus lastStatus, BuildActivity activity) {
-        if (activity == BuildActivity.BUILDING)
-            return lastStatus == BuildStatus.SUCCESS ? Color.YELLOW : Color.rgb(255, 128, 64);
-        return lastStatus == BuildStatus.SUCCESS ? Color.GREEN : Color.RED;
     }
 
     private int getResource(BuildStatus lastStatus, BuildActivity activity) {
