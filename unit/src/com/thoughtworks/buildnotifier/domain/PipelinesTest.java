10c3551d73fcc734deaf8785a7e043fdd46576c8
Garima Singh <garima@ThoughtWorks.(none)>
2010-09-16 14:23:33 +0530

    Adding unit tests to pipelines

--- unit/src/com/thoughtworks/buildnotifier/domain/PipelinesTest.java
+++ unit/src/com/thoughtworks/buildnotifier/domain/PipelinesTest.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.buildnotifier.domain;
 
-import com.thoughtworks.buildnotifier.model.domain.Pipeline;
-import com.thoughtworks.buildnotifier.model.domain.Pipelines;
+import com.thoughtworks.buildnotifier.model.domain.*;
 import org.junit.Test;
 
+import java.util.Date;
+
+import static com.thoughtworks.buildnotifier.model.domain.BuildActivity.*;
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 public class PipelinesTest {
 
@@ -13,5 +16,37 @@ public class PipelinesTest {
         Pipelines pipelines = new Pipelines();
         Pipeline pipeline = pipelines.find("project");
         assertNotNull(pipeline);
+    }
+
+    @Test
+    public void testIsFailingWhenOnePipelineISFailing() {
+        Pipelines pipelines = new Pipelines();
+        Pipeline pipeline1 = new Pipeline("Convoy");
+        Pipeline pipeline2 = new Pipeline("Regression");
+        Stage stage1 = new Stage("RSpec", SLEEPING, BuildStatus.FAILURE, "1234", new Date(), "http://localhost:4567/cctray.xml");
+        Stage stage2 = new Stage("RSpec", BUILDING, BuildStatus.SUCCESS, "1234", new Date(), "http://localhost:4567/cctray.xml");
+
+        pipeline1.addStage(stage1);
+        pipeline2.addStage(stage2);
+        pipelines.add(pipeline1);
+        pipelines.add(pipeline2);
+
+        assertEquals(pipelines.isFailing(), true);
+    }
+
+    @Test
+    public void testIsFailingWhenNoPipelineISFailing() {
+        Pipelines pipelines = new Pipelines();
+        Pipeline pipeline1 = new Pipeline("Convoy");
+        Pipeline pipeline2 = new Pipeline("Regression");
+        Stage stage1 = new Stage("RSpec", SLEEPING, BuildStatus.SUCCESS, "1234", new Date(), "http://localhost:4567/cctray.xml");
+        Stage stage2 = new Stage("RSpec", BUILDING, BuildStatus.SUCCESS, "1234", new Date(), "http://localhost:4567/cctray.xml");
+
+        pipeline1.addStage(stage1);
+        pipeline2.addStage(stage2);
+        pipelines.add(pipeline1);
+        pipelines.add(pipeline2);
+
+        assertEquals(pipelines.isFailing(), false);
     }
 }
